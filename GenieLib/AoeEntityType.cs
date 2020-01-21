43b92cb5c74b20e652d44bb196b18b9a88883198
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-01-21 20:17:34 +0100

    Unit types.

--- GenieLib/AoeEntityType.cs
+++ GenieLib/AoeEntityType.cs
@@ -4,28 +4,98 @@
     public enum AoeEntityType
     {
         Academy = 0,
+        Bowman = 4,
+        ImprovedBowman = 5,
+        CompositeBowman = 6,
+        Ballista = 11,
         Barracks = 12,
+        FishingBoat = 13,
+        TradeBoat = 15,
+        MerchantShip = 16,
         LightTransport = 17,
+        HeavyTransport = 18,
+        ScoutShip = 19,
+        WarGalley = 20,
+        Trireme = 21,
+        ElephantArcher = 25,
+        StoneThrower = 35,
+        Catapult = 36,
+        Cavalry = 37,
+        HeavyCavalry = 38,
+        HorseArcher = 39,
+        Chariot = 40,
+        ChariotArcher = 41,
+        WarElephant = 46,
         Elephant = 48,
         SiegeWorkshop = 49,
+        // Hero? = 58
         BerryBush = 59,
+        LionTame = 60,
+        // Hero? = 62
+        // Hero? = 63
         Deer = 65,
         GoldMine = 66,
-        GuardOrBallistaTower = 69,
+        GuardTower = 69,
         House = 70,
         SmallWall = 72,
+        Clubman = 73,
+        Axeman = 74,
+        ShortSwordsman = 75,
+        BroadSwordsman = 76,
+        LongSwordsman = 77,
+        // Hero? = 78
+        // Hero? = 81
         GovernmentCenter = 82,
         Villager = 83,
         Market = 84,
         ArcheryRange = 87,
+        Hoplite = 93,
+        Phalanx = 94,
         Stable = 101,
         StoneMine = 102,
         Temple = 104,
+        WarChest = 108,
         TownCenter = 109,
+        Flare = 112,
+        Priest = 125,
         Lion = 126,
         Fortification = 155,
         Ruins = 158,
+        Artifact = 159,
         DoublePoleFlag = 162,
+        // Hero? = 227
+        BlindLamePriest = 228,
+        InvisibleDemon = 229,
+        // Hero? = 230
+        // Hero? = 231
+        // Hero? = 233
+        Mercenary = 240,
+        // Hero? = 245
+        CatapultTrireme = 250,
+        Juggernaught = 277,
+        Helepolis = 279,
+        HeavyCatapult,
+        HeavyHorseArcher = 281,
+        Legion = 282,
+        Cataphract = 283,
+        Centurion = 291,
+        Raft = 292,
+        FlyingDutchman = 297,
+        Medusa = 298,
+        Scout = 299,
+        CamelRider = 338,
+        ScytheChariot = 339,
+        ArmoredElephant = 345,
+        Slinger = 347,
+        FireGalley = 360,
+        // Hero? = 375
+        // Hero? = 376
+        CleopatrasBarge = 377,
+        // Hero? = 378
+        // Hero? = 379
+        // Hero? = 381
+        // Hero? = 382
+
         /*fish = 260,
         Cliff1 = 264,
         Cliff2,
@@ -37,6 +107,5 @@
         Cliff8,
         Cliff9,
         Cliff10,*/
-        Scout = 299,
     }
 }
