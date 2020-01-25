52ae50a83be301420fae2461c699d4f0ee09bcc1
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-01-25 18:06:56 +0100

    Add Convert method.

---
+++ MapConverter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+using GenieLib;
+
+using War3Net.Build.Environment;
+using War3Net.Build.Info;
+using War3Net.Build.Widget;
+
+namespace ScenarioConverter
+{
+    public static class MapConverter
+    {
+        public static void Convert(string inputFile, string outputFolder)
+        {
+            var scenario = new Scenario(inputFile);
+            var map = scenario.Map;
+
+            if (scenario.FileVersion != AoeVersion.AOE1)
+            {
+                throw new NotSupportedException();
+            }
+
+            static uint RoundUpSize(int size)
+            {
+                return 1 + (uint)((size + 30) / 32) * 32;
+            }
+
+            var newWidth = RoundUpSize((int)map.width);
+            var newHeight = RoundUpSize((int)map.height);
+            var expandedMap = map.Expand(newWidth, newHeight, out var padding);
+
+            var minimap = MinimapGenerator.Generate(map);
+            var scaledBitmap = new Bitmap(256, 256);
+            using (var graphics = Graphics.FromImage(scaledBitmap))
+            {
+                graphics.DrawImage(minimap, 0, 0, 256, 256);
+            }
+
+            var mapEnvironment = MapEnvironmentGenerator.Generate(expandedMap);
+            mapEnvironment.Left = padding.Left * -128;
+            mapEnvironment.Bottom = padding.Bottom * -128;
+
+            if (!Directory.Exists(outputFolder))
+            {
+                Directory.CreateDirectory(outputFolder);
+            }
+
+            minimap.Save(Path.Combine(outputFolder, "minimap.png"));
+
+            var tga = TGASharpLib.TGA.FromBitmap(scaledBitmap, true);
+            tga.Save(Path.Combine(outputFolder, "war3mapMap.tga"));
+
+            mapEnvironment.SerializeTo(File.Create(Path.Combine(outputFolder, MapEnvironment.FileName)));
+            PathingMapGenerator.Generate(expandedMap).SerializeTo(File.Create(Path.Combine(outputFolder, PathingMap.FileName)));
+
+            MapDoodadsGenerator.Generate(scenario).SerializeTo(File.Create(Path.Combine(outputFolder, MapDoodads.FileName)));
+            MapUnitsGenerator.Generate(scenario).SerializeTo(File.Create(Path.Combine(outputFolder, MapUnits.FileName)));
+
+            var mapInfo = MapInfoGenerator.Generate(scenario, padding);
+            mapInfo.SerializeTo(File.Create(Path.Combine(outputFolder, MapInfo.FileName)));
+
+            using (var menuMinimapStream = File.Create(Path.Combine(outputFolder, "war3map.mmp")))
+            {
+                MenuMinimapGenerator.Generate(mapInfo, map.width, map.height, padding).CopyTo(menuMinimapStream);
+            }
+        }
+    }
+}
