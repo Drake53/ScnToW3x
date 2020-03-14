5ff5e37f14035d29a682da5bea13db769e6af7d0
Drake53 <49623303+Drake53@users.noreply.github.com>
2020-03-14 10:21:52 +0100

    Add Main.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace ScenarioConverter
+{
+    public class Program
+    {
+        private const string InputFolder = @"";
+        private const string OutputFolder = @"";
+
+        private static void Main()
+        {
+            foreach (var scenario in Directory.EnumerateFiles(InputFolder, "*.SCN", SearchOption.TopDirectoryOnly))
+            {
+                var inputFileInfo = new FileInfo(scenario);
+                var outputMapFolder = Path.Combine(OutputFolder, inputFileInfo.Name.Substring(0, inputFileInfo.Name.Length - inputFileInfo.Extension.Length));
+
+                MapConverter.Convert(scenario, outputMapFolder);
+            }
+        }
+    }
+}
